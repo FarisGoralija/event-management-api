26a869a8907fcda1af8a73b8e35ed89cb64eeaf7
Faris <farisg55@gmail.com>
2025-05-04 22:46:32 +0200

    Added dynamic relationship loading with CanLoadRelationships trait

--- event-management/app/Http/Controllers/Api/AttendeeController.php
+++ event-management/app/Http/Controllers/Api/AttendeeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Traits\CanLoadRelationships;
 use App\Http\Resources\AttendeeResource;
 use App\Models\Attendee;
 use App\Models\Event;
@@ -10,10 +11,16 @@ use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
 {
+    use CanLoadRelationships;
+
+    private array $relations = ['user'];
 
     public function index(Event $event)
     {
         $attendees = $event->attendees()->latest();
+         $attendees = $this->loadRelationships(
+             $event->attendees()->latest()
+         );
 
         return AttendeeResource::collection(
             $attendees->paginate()
@@ -22,9 +29,11 @@ class AttendeeController extends Controller
 
     public function store(Request $request, Event $event)
     {
-        $attendee = $event->attendees()->create([
-            'user_id' => 1
-        ]);
+        $attendee = $this->loadRelationships(
+            $event->attendees()->create([
+                'user_id' => 1
+            ])
+        );
 
         return new AttendeeResource($attendee);
     }
@@ -32,7 +41,9 @@ class AttendeeController extends Controller
 
     public function show(Event $event, Attendee $attendee)
     {
-        return new AttendeeResource($attendee);
+        return new AttendeeResource(
+            $this->loadRelationships($attendee)
+        );
     }
 
     /**
